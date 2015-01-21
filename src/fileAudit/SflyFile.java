7e22d0c1e23072f85b726e28d21f15bf033c073d
JoolsF <joolsmf@gmail.com>
2015-01-21 17:23:31 +0000

    updated file for new data formats

--- src/fileAudit/SflyFile.java
+++ src/fileAudit/SflyFile.java
@@ -1,14 +1,20 @@
 package fileAudit;
+
+import java.util.ArrayList;
+
 public class SflyFile{
 	int bin;
 	int mp3;
 	int xml;
 	int guideWav;
 	int masterWav;
+	int kbp;
 	int mp4;
 	int unMatched;
+	ArrayList<String> unmatchedData;
 	
 	public SflyFile(String fileName){
+		unmatchedData = new ArrayList<String>();
 		addFile(fileName);
 	}
 	
@@ -20,16 +26,21 @@ public class SflyFile{
 			mp3 ++;
 		} else if(fileName.contains(".xml")){
 			xml ++;
-		} else if(fileName.contains("_Guide.wav")){
+		} else if(fileName.contains("Guide")){
 			guideWav ++;
-		} else if (fileName.contains("_Master.wav")){
+		} else if (fileName.contains("_Master") || fileName.contains("_master")){
 			masterWav ++;
 		} else if (fileName.contains(".MP4")){
 			mp4++;
-		} else{
+		}else if (fileName.contains(".kbp")){
+			kbp++;
+		}else{
 			unMatched ++;
+			unmatchedData.add(fileName);
 		}
 	}
+	
+	
 	
 	public String getFileData(){
 		return 	"bin:, " + bin + 
@@ -37,8 +48,10 @@ public class SflyFile{
 				", xml:, " + xml + 
 				", guide Wav:, " + guideWav + 
 				", masterWav:, " + masterWav + 
+				", kbp:, " + kbp +
 				",  mp4:, " + mp4 + 
-				", unMatched:, " + unMatched;
+				", unMatched:, " + unMatched +
+				", unmatchedData:, " + unmatchedData.toString();
 	}
 
 }
