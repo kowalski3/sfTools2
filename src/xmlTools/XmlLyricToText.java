5d4c3e8fb8f26d7e3a0b011d2d26c3ee87b209fa
julian <julian@Julian-PC.sunflykaraoke.local>
2015-12-17 16:50:58 +0000

    Amendments

--- src/xmlTools/XmlLyricToText.java
+++ src/xmlTools/XmlLyricToText.java
@@ -2,12 +2,15 @@ package xmlTools;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -39,29 +42,37 @@ public class XmlLyricToText {
 	}
 	
 	
-	private void getLyricsInXml(String fileName)throws Exception {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setNamespaceAware(true); // never forget this!
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document doc = builder.parse(fileName);
-		
-		XPathFactory xpathfactory = XPathFactory.newInstance();
-		XPath xpath = xpathfactory.newXPath();
-		
-		// 1) Get all Text Elements
-		XPathExpression expr = xpath.compile("//Text/text()");
-		Object result = expr.evaluate(doc, XPathConstants.NODESET);
-		NodeList nodes = (NodeList) result;
-		
-		ArrayList<String> songLyrics =  new ArrayList<String>();
-		for (int i = 0; i < nodes.getLength(); i++) {
-			if(containsLyrics(nodes.item(i).getNodeValue())){
-				songLyrics.add(nodes.item(i).getNodeValue());
-			}
+	private void getLyricsInXml(String fileName)  {
+	
+		try{
 			
-		}
+		
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true); // never forget this!
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(fileName);
+			
+			XPathFactory xpathfactory = XPathFactory.newInstance();
+			XPath xpath = xpathfactory.newXPath();
+			
+			// 1) Get all Text Elements
+			XPathExpression expr = xpath.compile("//Text/text()");
+			Object result = expr.evaluate(doc, XPathConstants.NODESET);
+			NodeList nodes = (NodeList) result;
+			
+			ArrayList<String> songLyrics =  new ArrayList<String>();
+			for (int i = 0; i < nodes.getLength(); i++) {
+				if(containsLyrics(nodes.item(i).getNodeValue())){
+					songLyrics.add(nodes.item(i).getNodeValue());
+				}
+				
+			}
 		
 		writeToFile(fileName, songLyrics);
+		} catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
+			System.out.println(e);
+			
+		}
 	}
 	
 	public boolean containsLyrics(String lyricLine){
@@ -73,18 +84,24 @@ public class XmlLyricToText {
 		
 	}
 	
-	private void traverseFolders(String startDirectory) throws Exception{
+	private void traverseFolders(String startDirectory) {
 	
 		File directory = new File(startDirectory);
-
+		
 		// get all the files from a directory
 		File[] fList = directory.listFiles();
 		
 		for (File file : fList) {
+			//System.out.println(file.getName());
 			if (file.isFile()) {
 				
 				if(file.getName().contains(".xml")) {
-					getLyricsInXml(file.getAbsolutePath());
+					try {
+						getLyricsInXml(file.getAbsolutePath());
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						System.out.println("ERROR 1");
+					}
 				}
 				
 			} else if (file.isDirectory()) {
@@ -94,7 +111,7 @@ public class XmlLyricToText {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		new XmlLyricToText().traverseFolders("C:\\Users\\Julian.SUNFLYKARAOKE\\Desktop\\input");
+		new XmlLyricToText().traverseFolders("C:\\Users\\JULIAN.SUNFLYKARAOKE\\Desktop\\xmltotext");
 	}
 }
 
